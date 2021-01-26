6afd7b8487132d7eda483361bfce1d8e38903a09
Chenyang Ni <chenyangni@Chenyangs-MBP.fios-router.home>
2021-01-25 23:25:26 -0500

    finish the entire project

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSongIndex from '../queries/fetchSongIndex';
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component{
   render() {
@@ -9,11 +11,12 @@ class SongDetail extends Component{
 
     if (!song) { return <div>Loading...</div>}
 
-    console.log(this.props);
     return(
       <div>
         <Link to='/'>Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics}/>
+        <LyricCreate songId={this.props.params.id}/>
       </div>
     );
   }
